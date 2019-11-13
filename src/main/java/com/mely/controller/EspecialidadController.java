96244742899c42ccf5044e4c4e459a875f95fea0
Melyna <mebipeos@outlook.com>
2019-11-13 18:15:01 -0500

    add delete especialista

--- src/main/java/com/mely/controller/EspecialidadController.java
+++ src/main/java/com/mely/controller/EspecialidadController.java
@@ -2,8 +2,15 @@ package com.mely.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,4 +27,22 @@ public class EspecialidadController {
 	public List<Especialidad> listar(){
 		return service.listar();
 	}
+	
+	@PostMapping
+	public void register(@Valid @RequestBody Especialidad esp) {
+		service.registrar(esp);
+	}
+	
+	@PutMapping
+	public void modificar(@Valid @RequestBody Especialidad esp) {
+		service.modificar(esp);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void eliminar(@PathVariable("id")  Integer id) {
+		service.eliminar(id);
+		
+	}
+	
+
 }
